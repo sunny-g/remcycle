cd30af0c9e6e6d9f54526a8213931430e2071ca4
Sunny G <sunny.gonna@gmail.com>
2017-04-25 17:46:15 -0700

    adds isolate

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import addActionHandlers from './hoc/addActionHandlers';
+import isolate from './hoc/isolate';
 import mapProps from './hoc/mapProps';
 
 import createComponent from './createComponent';
 
 export { addActionHandlers };
+export { isolate };
 export { mapProps };
 
 export default createComponent;
