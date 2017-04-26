694f7ca62b444efb9631d998f3d5e17e86a376aa
Sunny G <sunny.gonna@gmail.com>
2017-04-26 10:30:51 -0700

    improves mapProps tests

--- test/hoc/mapProps.test.ts
+++ test/hoc/mapProps.test.ts
@@ -6,24 +6,23 @@ import mapProps from '../../src/hoc/mapProps';
 describe('mapProps HOC', () => {
 
   test('should only include props returned from mapper', done => {
+    function main({ props }) {
+      return { props };
+    }
+
     const props$ = of({ test1: true, test2: false });
 
     const hoc = mapProps(props => ({
-      ...props,
       test2: true,
     }));
 
-    function main({ props: propsSource }) {
-      propsSource
-        .tap(props => expect(props.test1).toBe(true))
-        .tap(props => expect(props.test2).toBe(true))
-        .tap(props => expect(props.test3).toBeUndefined())
-        .observe(done);
+    const sinks = hoc(main)({ props: props$ });
 
-      return {};
-    }
-
-    return hoc(main)({ props: props$ });
+    sinks.props
+      .tap(props => expect(props.test1).toBeUndefined())
+      .tap(props => expect(props.test2).toBeDefined())
+      .tap(props => expect(props.test2).toBe(true))
+      .observe(done);
   });
 
 });
