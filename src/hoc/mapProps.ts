67f903410a81c9aca63143e877ff08bc02302c08
Sunny G <sunny.gonna@gmail.com>
2017-04-26 15:40:34 -0700

    update mapProps typings

--- src/hoc/mapProps.ts
+++ src/hoc/mapProps.ts
@@ -1,14 +1,17 @@
 import { of } from 'most';
 import mapSources from '@sunny-g/cycle-utils/es2015/mapSources';
 import { HigherOrderComponent } from '@sunny-g/cycle-utils/es2015/interfaces';
+import { shallowEquals } from '../util';
 
 export interface MapProps {
-  (mapper: ((props: {}) => {})): {};
+  (mapper: ((props: {}) => {})): HigherOrderComponent;
 }
 
-const mapProps: MapProps = (mapper): HigherOrderComponent => mapSources(
+const mapProps: MapProps = (mapper) => mapSources(
   'props', (propsSource = of({})) => ({
-    props: propsSource.map(mapper),
+    props: propsSource
+      .map(mapper)
+      .skipRepeatsWith(shallowEquals),
   }),
 );
 
