c465c5891986fb6d6fe79ac55a8cf08caf45c9ef
Sunny G <sunny.gonna@gmail.com>
2017-06-22 17:10:10 -0700

    minor reduxSinksCombiner syntax update

--- src/reduxSinksCombiner.ts
+++ src/reduxSinksCombiner.ts
@@ -4,10 +4,10 @@ import { mapObj } from './util';
 const reduxSinksReducer = (newReduxSink, reduxSink) =>
   newReduxSink.combine((newReduxSinkObj, reduxSinkObj) => ({
     ...newReduxSinkObj,
-    ...mapObj((action$, actionType) => newReduxSinkObj.hasOwnProperty(actionType)
+    ...mapObj((action$, actionType) => (newReduxSinkObj.hasOwnProperty(actionType)
       ? newReduxSinkObj[actionType].merge(action$)
       : action$
-    )(reduxSinkObj),
+    ))(reduxSinkObj),
   }), reduxSink);
 
 const reduxSinksCombiner = (...reduxSinks) => reduxSinks.reduce(reduxSinksReducer, of({}));
