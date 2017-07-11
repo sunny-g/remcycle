7bae023315c2a1c8025750db5d05568b26d8af06
Sunny G <sunny.gonna@gmail.com>
2017-07-11 16:32:27 -0700

    withState supports watching prop change events as reducers

--- src/hoc/withState.ts
+++ src/hoc/withState.ts
@@ -1,4 +1,4 @@
-import { of, merge } from 'most';
+import { of, merge, mergeArray } from 'most';
 import { hold } from '@most/hold';
 import mapSources from '@sunny-g/cycle-utils/es2015/mapSources';
 import { HigherOrderComponent } from '@sunny-g/cycle-utils/src/interfaces';
@@ -7,59 +7,80 @@ import { pick, shallowEquals } from '../util';
 export interface WithState {
   ( propName: string,
     initialState: any,
-    reducers: { [type: string]: (state: any, action: any, props: any) => any; }
+    actionReducers: { [type: string]: (state: any, action: any, props: any) => any; },
+    propReducers: { [type: string]: (state: any, props: any) => any; },
   ): HigherOrderComponent;
 }
 
-const withState: WithState = (propName, initialState, reducers) => mapSources(
-  [ 'REDUX', 'props' ],
-  (REDUX, propsSource = of({})) => {
-    // pass through if:
-      // this was distinct from before
-    // pass through only when every other prop was the same as before AND this one was different
-      // aka, skip if `isComplete` is the same AND
-    const propReducer$ = propsSource
-      .map(props => props[propName])
-      .filter(prop => prop !== undefined)
-      .skipRepeatsWith(shallowEquals)
-      .map(prop => state => prop);
+const withState: WithState = (propName, initialState, actionReducers, propReducers = {}) =>
+  mapSources(
+    [ 'REDUX', 'props' ],
+    (REDUX, propsSource = of({})) => {
+      // TODO: pass through if:
+        // ? this was distinct from before
+        // ? when every other prop was the same as before AND this one was different
+      const overwriteReducer$ = propsSource
+        .map(props => props[propName])
+        .filter(prop => prop !== undefined)
+        .skipRepeatsWith(shallowEquals)
+        .map(prop => state => prop);
 
-    const reducer$ = merge(
-      propReducer$,
-      ...Object
-        .keys(reducers)
-        .map(actionType => {
-          const action$ = REDUX.action.select(actionType);
+      const watchedPropReducer$ = mergeArray(
+        Object
+          .keys(propReducers)
+          .map(_watchedPropsNames => {
+            const propReducer = propReducers[_watchedPropsNames];
+            const watchedPropsNames = _watchedPropsNames.split(',');
+            const watchedProps$ = propsSource
+              .map(pick(watchedPropsNames))
+              .skipRepeatsWith(shallowEquals)
+              .constant(null);
 
-          return action$
-            .sample((action, props) =>
-              state => reducers[actionType](state, action, props),
-            action$, propsSource);
-        }),
-    );
+            return watchedProps$
+              .sample(props =>
+                state => propReducer(state, props),
+              propsSource);
+          }),
+      );
 
-    const state$ = (typeof initialState === 'function')
-      ? (function() {
-        const defaultState = Symbol('=== default withState state ===');
-        const initialStateReducer$ = propsSource
-          .take(1)
-          .map(props => _ => initialState(props));
+      const reducer$ = merge(
+        overwriteReducer$,
+        watchedPropReducer$,
+        ...Object
+          .keys(actionReducers)
+          .map(actionType => {
+            const actionReducer = actionReducers[actionType];
+            const action$ = REDUX.action.select(actionType);
 
-        return initialStateReducer$
-          .concat(reducer$)
-          .scan((state, reducer: (any: any) => any) => reducer(state), defaultState)
-          .filter(state => state !== defaultState);
-      })()
-      : reducer$
-        .scan((state, reducer: (any: any) => any) => reducer(state), initialState);
+            return action$
+              .sample((action, props) =>
+                state => actionReducer(state, action, props),
+              action$, propsSource);
+          }),
+      );
 
-    const props$ = propsSource
-      .combine((props, state) => ({ ...props, [propName]: state }), state$)
-      .skipRepeatsWith(shallowEquals)
-      .thru(hold);
+      const state$ = (typeof initialState === 'function')
+        ? (function() {
+          const defaultState = Symbol('=== default withState state ===');
+          const initialStateReducer$ = propsSource
+            .take(1)
+            .map(props => _ => initialState(props));
 
-    return { props: props$ };
-  },
-);
+          return initialStateReducer$
+            .concat(reducer$)
+            .scan((state, reducer: (any: any) => any) => reducer(state), defaultState)
+            .filter(state => state !== defaultState);
+        })()
+        : reducer$
+          .scan((state, reducer: (any: any) => any) => reducer(state), initialState);
+
+      const props$ = propsSource
+        .combine((props, state) => ({ ...props, [propName]: state }), state$)
+        .skipRepeatsWith(shallowEquals)
+        .thru(hold);
+
+      return { props: props$ };
+    },
+  );
 
 export default withState;
