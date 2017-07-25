551140e2e9981508eb74aebabae5e7ab148347a3
Sunny G <sunny.gonna@gmail.com>
2017-07-24 17:28:43 -0700

    update logActions

--- src/hoc/logActions.ts
+++ src/hoc/logActions.ts
@@ -3,10 +3,10 @@ import mapSinks from '@sunny-g/cycle-utils/es2015/mapSinks';
 import { HigherOrderComponent } from '@sunny-g/cycle-utils/src/interfaces';
 import { mapObj } from '../util';
 
-const logActions = (sinkLogger = () => {}, actionLoggers = {}): HigherOrderComponent => mapSinks(
+const logActions = (sinkLogger, actionLoggers = {}): HigherOrderComponent => mapSinks(
   'REDUX', (REDUX = of({})) => ({
     REDUX: REDUX
-      .tap(sinkLogger)
+      .tap((typeof sinkLogger === 'function') ? sinkLogger : () => {})
       .map(mapObj((action$, actionType) => {
         if (actionLoggers.hasOwnProperty(actionType)) {
           action$.observe(actionLoggers[actionType]);
