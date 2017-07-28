18f79c1d1c7508b086bb7b6c820d9f566f035efc
Sunny G <sunny.gonna@gmail.com>
2017-07-28 16:57:41 -0700

    update mapView tests

--- test/hoc/mapView.test.ts
+++ test/hoc/mapView.test.ts
@@ -13,7 +13,7 @@ describe('mapView HOC', () => {
   const HelloComponent = fromReactDOMComponent('REACT', Hello);
 
   test('should wrap a React vtree with other React elements', done => {
-    const hoc = mapView((_, vtree) =>
+    const hoc = mapView(vtree =>
       createElement('div', null, vtree)
     );
 
